72eb36e1363e524d513cd4e41e7af9256bf4a022
Cristina <S00184388@mail.itsligo.ie>
2019-01-09 10:02:46 -0800

    Project Stucture Done

---
+++ Exam3PartialRad/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exam3PartialRad.MvcApplication" Language="C#" %>
---
+++ Exam3PartialRad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Exam3PartialRad.Startup))]
+namespace Exam3PartialRad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
